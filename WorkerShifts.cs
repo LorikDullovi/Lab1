84f60c9df0d56afa51307b256127079a9f222e2a
ArmendDullovi <adullovi01@gmail.com>
2022-06-19 20:15:49 +0200

    WorkerShifts.cs

---
+++ WorkerShifts.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class WorkerShifts
+    {
+        public int WorkerShiftsId { get; set; }
+
+        public string ParaditeName { get; set; }
+
+        public string Pasdite { get; set; }
+
+        public string NdrrimNate { get; set; }
+
+    }
+}
