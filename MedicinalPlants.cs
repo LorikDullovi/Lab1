107165d724fa2eded8e63c46e8c9eccfc84c2f66
LorikDullovi <lorikdullovi@gmail.com>
2022-06-19 10:25:17 +0200

    MedicianlPlantsController

---
+++ MedicinalPlants.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class MedicinalPlants
+    {
+        public int MedicinalPlantsId { get; set; }
+
+        public string MedicinalPlantsName { get; set; }
+
+        public string Functionn { get; set; }
+
+        public string Color { get; set; }
+
+
+    }
+}
