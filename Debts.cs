1f6399a17cc4e8cbe7e287db08e7dfae201aed72
ArmendDullovi <adullovi01@gmail.com>
2022-06-19 20:22:16 +0200

    Debts.cs

---
+++ Debts.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Debts
+    {
+        public int DebtsId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Debt { get; set; }
+    }
+}
